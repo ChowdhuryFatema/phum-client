8042192a962a348fbbddc1f531faa98c082b2f21
ChowdhuryFatema <fatema7chowdhury.ctg@gmail.com>
2026-07-14 20:28:31 +0600

    Refactor routes with util function

---
+++ src/pages/student/StudentDashboard.tsx
@@ -0,0 +1,11 @@
+
+
+const StudentDashboard = () => {
+    return (
+        <div>
+            <h1>This is Student Dashboard Page</h1>
+        </div>
+    );
+};
+
+export default StudentDashboard;
